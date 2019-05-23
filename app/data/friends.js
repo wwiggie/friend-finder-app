be70e404f0e6af3718292ba7701c38bac9d6b7ea
Wendy Wigg <wiggwendy@gmail.com>
2019-05-23 10:21:14 -0700

    storing new users, modal are working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,9 @@
 // data
 var friends = [
     {
-        "name": "Agi",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
+        name: "Agi",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores:[
             5,
             1,
             4,
@@ -14,6 +14,38 @@ var friends = [
             5,
             4,
             1 
+        ]
+    },
+    {
+        name: "Jessie",
+        photo: "https://pm1.narvii.com/6523/078fe3d9797190f15437eff9d66922e6a074938c_hq.jpg",
+        scores: [
+            3,
+            3,
+            1,
+            5,
+            1,
+            5,
+            3,
+            1,
+            3,
+            5
+        ]
+    },
+    {
+        name: "James",
+        photo: "https://i.pinimg.com/originals/9e/2e/b6/9e2eb6dbce48c4c03b5e8a723d06d2d7.jpg",
+        scores: [
+            5,
+            1,
+            2,
+            5,
+            1,
+            3,
+            5,
+            1,
+            1,
+            1
         ]
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,7 +10,31 @@ module.exports = function(app) {
 
     // api POST request - used to handle incoming survey results. Also handles compatibility logic
     app.post("/api/friends", function(req, res) {
-        console.log(req.body);
-    });
+        console.log(req.body.scores);
 
+        var user = req.body;
+        for (var i = 0; i < user.scores.length; i++) {
+            user.scores[i] = parseInt(user.scores[i]);
+        }
+
+        var bestFriendIndex = 0;
+        var minimumDifference = 40;
+
+        for (var i = 0; i < friendsData.length; i++) {
+            var totalDifference = 0;
+            for (var j = 0; j <friendsData[i].scores.length; j++) {
+                var difference = Math.abs(user.scores[j] - friendsData[i].scores[j]);
+                totalDifference += difference;
+            }
+
+            if (totalDifference < minimumDifference) {
+                bestFriendIndex = i;
+                minimumDifference = totalDifference;
+            }
+        }
+
+        friendsData.push(user);
+
+        res.json(friendsData[bestFriendIndex]);
+    });
 };
